0311dfad99971e8ed1c33b8b0d5555a4f6ea3348
AishwaryaBPatil <patilaishwarya9168@gmail.com>
2018-06-17 09:35:29 +0530

    Started Project

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+app.listen(port);
+app.get('*', function(req, res) {
+        res.sendfile('index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
